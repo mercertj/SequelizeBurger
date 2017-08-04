917d69370b8a1b8a0fe19658a1da57d481f46b73
Thomas Mercer <tmercer74@gmail.com>
2017-08-03 19:06:23 -0700

    updated files. prepping for heroku

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,31 @@
-var orm = require("../config/orm.js");
+module.exports = function(sequelize, DataTypes) {
 
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(name, cb) {
-    orm.create("burgers", [
-      "burger_name", "devoured"
-    ], [
-      name, false
-    ], cb);
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
+  const newBurgers = sequelize.define("burgers" , {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160]
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    burger_time: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  });
+
+  newBurgers.associate = models => {
+    newBurgers.belongsTo(models.Author, {
+      foreignKey: {
+        allowNull: false
+      }
+    })
   }
-};
 
-module.exports = burger;
+  return newBurgers;
+};
